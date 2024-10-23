9211698bc22fdb5a395d18485e280a3d210cfe88
Ulf Haga <ulf.haga@gmail.com>
2024-10-23 21:16:58 +0200

    Saving

--- examples/processingloaddataset1.py
+++ examples/processingloaddataset1.py
@@ -1,7 +1,16 @@
 from datasets import load_dataset
-
+print("Starting")
 raw_datasets = load_dataset("glue", "mrpc");
+print(raw_datasets)
+raw_train_dataset = raw_datasets["train"]
+print("\n---0---\n")
+print(raw_train_dataset[0])
+print("\n---1---\n")
+print(raw_train_dataset[1])
+print("\n---2---\n")
+print(raw_train_dataset[2])
+print("\n---3---\n")
+print(raw_train_dataset[3])
 
-raw_datasets;
-
-
+print("\n---features---\n")
+print(raw_train_dataset.features)
