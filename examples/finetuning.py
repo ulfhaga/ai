049592dca2057640a83f50971e0239eac9c28364
Ulf Haga <ulf.haga@gmail.com>
2024-10-27 19:35:41 +0100

    Saving

---
+++ examples/finetuning.py
@@ -0,0 +1,48 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+from transformers import TrainingArguments
+from transformers import AutoModelForSequenceClassification
+from transformers import Trainer
+import numpy as np
+import evaluate
+
+raw_datasets = load_dataset("glue", "mrpc")
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+
+training_args = TrainingArguments("test-trainer")
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+# To fine-tune the model on our dataset, we just have to call the train() method of our Trainer:
+trainer.train()
+
+predictions = trainer.predict(tokenized_datasets["validation"])
+print(predictions.predictions.shape, predictions.label_ids.shape)
+
+
+
+preds = np.argmax(predictions.predictions, axis=-1)
+
+
+
+metric = evaluate.load("glue", "mrpc")
+metric.compute(predictions=preds, references=predictions.label_ids)
