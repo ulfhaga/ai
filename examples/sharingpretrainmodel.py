470f4f34c8931d857268442169d0fb17fc7c27c6
Ulf Haga <ulf.haga@gmail.com>
2024-12-29 13:16:18 +0100

    Saving

--- examples/sharingpretrainmodel.py
+++ examples/sharingpretrainmodel.py
@@ -20,8 +20,8 @@ checkpoint = "camembert-base"
 model = AutoModelForMaskedLM.from_pretrained(checkpoint)
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
-model.save_pretrained("<path_to_dummy_folder>")
-tokenizer.save_pretrained("<path_to_dummy_folder>")
+model.save_pretrained("/tmp/dummy")
+tokenizer.save_pretrained("/tmp/dummy")
 
 '''
 model.push_to_hub("dummy-model")
